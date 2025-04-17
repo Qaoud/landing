f08673d5f9e4ae49fbfcf0a96a3d1491b857a828
Qaoud <mohamed.qaoud@outlook.com>
2025-04-17 19:21:08 +0200

    Fix social media icons and position 3D loader in the middle of the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
 import './App.css'
 
 function App() {
+  const handleMouseMove = (e) => {
+    const cloud = document.querySelector('.cloud-shape');
+    if (cloud) {
+      const xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+      const yAxis = (window.innerHeight / 2 - e.pageY) / 50;
+      cloud.style.transform = `translate(-50%, -50%) rotate(3deg) skew(2deg, -1deg) translate(${xAxis}px, ${yAxis}px)`;
+    }
+  };
+
   return (
-    <div className="container">
+    <div className="container" onMouseMove={handleMouseMove}>
+      {/* Logo in the top left */}
+      <div className="logo">DeepRevel</div>
+
+      {/* Social links in the top right */}
+      <div className="social-links">
+        <a href="https://twitter.com/deeprevel" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src={process.env.PUBLIC_URL + '/x_logo.png'} alt="X (Twitter)" className="social-logo" />
+        </a>
+        <a href="https://instagram.com/deeprevel" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src={process.env.PUBLIC_URL + '/instagram_logo.png'} alt="Instagram" className="social-logo" />
+        </a>
+        <a href="https://linkedin.com/company/deeprevel" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src={process.env.PUBLIC_URL + '/linkedin_logo.png'} alt="LinkedIn" className="social-logo" />
+        </a>
+      </div>
+
+      {/* 3D Boxes Loader */}
+      <div className="boxes-container">
+        <div className="boxes">
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+
       <div className="content">
-        <h1 className="title">DeepRevel</h1>
+        <div className="cloud-shape"></div>
         <p className="tagline">Brewing brilliance behind the scenes… stay tuned!</p>
       </div>
     </div>
